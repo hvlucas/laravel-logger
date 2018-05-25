5a648eed7fdf8797b84bdd55bf11ae222d3eb129
Lucas Novoa <lucas@highervisibility.com>
2018-05-25 09:52:26 -0500

    route and controller setup

---
+++ src/app/Http/Controllers/LaravelLoggerController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HVLucas\LaravelLogger\App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class LaravelLoggerController extends Controller
+{
+    public function __construct()
+    {
+    }
+}
---
+++ src/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+Route::group(['prefix' => config('laravel_logger.route_prefix', 'events'), 'namespace' => 'HVLucas\LaravelLogger\App\Http\Controllers'], function(){
+    //TODO
+    //write routes
+});
